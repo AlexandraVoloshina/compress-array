41347384ee878c0236b56d9fa013d43360647e8c
Alexandra <alexandrita@ukr.net>
2019-07-15 22:33:16 +0300

    Add

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
 var arr = [0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1];
 var arr1 = [0,0,1,1,1,1,0,1,1,1];
 
-var resultArr = [];
-var k_1 = 0;
-var k_2 = 0;
-for (var i = 0; i < arr.length ; i++) {
-	if (arr[i] === 1) {
-		if (k_2) {
-			resultArr.push(k_2);
+function compressArr(arr){
+var sum_element = 1, z = 0, resultArr = [];
+	for (var i=0; i<arr.length; i++){
+		if(arr[i] === arr[i+1]){
+			sum_element++;
+		} else {
+			resultArr[z] = sum_element;
+			z++;
+			sum_element=1;
 		}
-		k_1 ++;
-		k_2 = 0;
 	}
-	if(arr[i] === 0) {
-		if (k_1) {
-			resultArr.push(k_1);
-		}
-		k_2++;
-		k_1 = 0;
-	}		
+	return resultArr;	
 }
-if (k_1) {
-	resultArr.push(k_1);
-}
-if (k_2) {
-	resultArr.push(k_2);
-}
-console.log(resultArr);
+
+console.log(compressArr(arr));
+
